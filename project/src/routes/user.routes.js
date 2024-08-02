9a9eb4c1a1b77d196cfe23d564262d788a83a70b
Pradeep Sahu <official.pradeepsahu@gmail.com>
2024-08-02 19:16:46 +0530

    Add refreshToken endpoint

--- project/src/routes/user.routes.js
+++ project/src/routes/user.routes.js
@@ -5,11 +5,14 @@ import {
   loginUser,
   registerUser,
   logoutUser,
+  refreshAccessTokens,
 } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
+//router.route(routeName,middleWare,controller)
 router.route("/register").post(
   upload.fields([
     { name: "avatar", maxCount: 1 },
@@ -22,5 +25,7 @@ router.route("/login").post(loginUser);
 
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
+
+router.route("/refresh-Token").post(refreshAccessTokens);
 
 export default router;
